c229585a45cb1a9676223800635de0d47968bd72
Jonathan Emminizer <jonathan.emminizer@yahoo.com>
2025-01-11 15:35:18 -0500

    Added Phoenix 6 Vender Dep and made the full test example with a Spark Max.

--- src/test/java/frc/robot/subsystems/TestExample.java
+++ src/test/java/frc/robot/subsystems/TestExample.java
@@ -7,7 +7,31 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.revrobotics.sim.SparkMaxSim;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.math.system.plant.DCMotor;
+
 public class TestExample {
-    private static final double DELTA = 1e-2;
-    
+    private static final double DELTA = 1e-2; // Acceptable delta time
+    SparkMaxSim m_simMotor;
+
+    @BeforeEach // Runs before each test
+    void setup() {
+        assert HAL.initialize(500, 0);
+        m_simMotor = new SparkMaxSim(new SparkMax(1, MotorType.kBrushless), new DCMotor(1, 1, 1, 1, 1, 0));
+    }
+
+    @AfterEach // Runs after each test
+    void shutdown() {
+        m_simMotor = null;
+    }
+
+    @Test // Is a test, runs on build
+    void exampleTest() {
+        m_simMotor.setBusVoltage(120);
+        assertEquals(m_simMotor.getBusVoltage(), 120.0);
+    }
 }
