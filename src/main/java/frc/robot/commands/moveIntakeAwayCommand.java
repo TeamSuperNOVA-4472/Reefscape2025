995931dd3487cf1a27c0d96b3a097c10b290a651
Sebastian Diaz <153384648+ZotetheMighty09@users.noreply.github.com>
2025-01-26 15:30:47 -0500

    added a testing processor auto

---
+++ src/main/java/frc/robot/commands/moveIntakeAwayCommand.java
@@ -0,0 +1,12 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class moveIntakeAwayCommand extends SequentialCommandGroup
+{
+        public moveIntakeAwayCommand()
+        {
+            
+        }
+}
