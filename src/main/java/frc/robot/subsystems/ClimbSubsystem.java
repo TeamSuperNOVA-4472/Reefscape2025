863ddd8f90a3c62df0b0de5516d011099db506f8
FarisFarag <farisfarag452@yahoo.com>
2025-02-01 10:25:02 -0500

    Updated climb subsystem with a grabber which includes voltage and such.

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -8,45 +8,75 @@ import frc.robot.Robot;
 // TODO: Actual motor ports
 public class ClimbSubsystem extends SubsystemBase
 {
+
+    // THE CONSTANTS
+    private static final int LEFT_MOTOR = 0;
+    private static final int RIGHT_MOTOR = 0;
+    private static final int GRABBER_MOTOR = 0;
+
     private final PWMTalonFX mLeftClimbMotor;
     private final PWMTalonFX mRightClimbMotor;
+    private final PWMTalonFX mGrabberMotor;
 
-    private double mCurrentVoltage = 0; // Starts at 0 voltage!
+    private double mClimbVoltage = 0;
+    private double mGrabberVoltage = 0;
 
     // Constructor
     public ClimbSubsystem()
     {
-        mLeftClimbMotor = new PWMTalonFX(1);
-        mRightClimbMotor = new PWMTalonFX(0); // Two krakens using TalonFX motor controllers
+        mLeftClimbMotor = new PWMTalonFX(LEFT_MOTOR); // Move these to constants at the top, DONE
+        mRightClimbMotor = new PWMTalonFX(RIGHT_MOTOR); // Two krakens using TalonFX motor controllers
+        mGrabberMotor = new PWMTalonFX(GRABBER_MOTOR);  // Two krakens using TalonFX motor controllers
     }
 
-    public void setVoltage(double pNewVoltage)
+    public void setClimbVoltage(double pNewVoltage)
     {
-        mCurrentVoltage = pNewVoltage;
+        mClimbVoltage = pNewVoltage;
 
         if(Robot.isReal())
         {
-            updateVoltage();
+            updateClimbVoltage();
+        }
+    }
+
+    public void setGrabberVoltage(double pNewVoltage)
+    {
+        mGrabberVoltage = pNewVoltage;
+
+        if (Robot.isReal())
+        {
+            updateGrabberVoltage();
         }
     }
 
     // Private update method. idk if this is required but I think it's good practice - and makes life easier!
-    private void updateVoltage()
+    private void updateClimbVoltage()
     {
-        mLeftClimbMotor.setVoltage(mCurrentVoltage);
-        mRightClimbMotor.setVoltage(mCurrentVoltage);
+        mLeftClimbMotor.setVoltage(mClimbVoltage);
+        mRightClimbMotor.setVoltage(mClimbVoltage);
+    }
+
+    private void updateGrabberVoltage()
+    {
+        mGrabberMotor.setVoltage(mGrabberVoltage);
     }
 
     // Gets the voltage of the climb subsystem.
-    public double getVoltage()
+    public double getClimbVoltage()
     {
-        return mCurrentVoltage;
+        return mClimbVoltage;
+    }
+
+    public double getGrabberVoltage()
+    {
+        return mGrabberVoltage;
     }
 
     // Run constantly to ensure that voltage is what it should be
     @Override
     public void periodic()
     {
-        SmartDashboard.putNumber("Climb Voltage", mCurrentVoltage);
+        SmartDashboard.putNumber("Climb Voltage", mClimbVoltage);
+        SmartDashboard.putNumber("Grabber Voltage", mGrabberVoltage);
     }
 }
