80be8ec8bda24dce81f8ab119f0dba98ddabe742
Troy Maurer <troytmaurer@outlook.com>
2025-02-24 17:20:09 -0500

    Fixing the use of IntakePresets, which has been removed.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -19,7 +19,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
-import frc.robot.objectmodels.IntakePresets;
 import frc.robot.objectmodels.LightState;
 import frc.robot.objectmodels.LightStatusRequest;
 
